de7d7e54c7dd9c69a61da6fb9d8434c25467edc1
naimarais <137940593+naimarais@users.noreply.github.com>
2023-09-12 16:22:07 -0500

    Update Final_Project_Dash.py

--- Final_Project_Dash.py
+++ Final_Project_Dash.py
@@ -66,7 +66,7 @@ def update_input_container(selected_statistics):
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
     Output(component_id='output-container', component_property='children'),
-    [Input(component_id='select-year', component_property='value'), Input(component_id='dropdown-statistics', component_property='value')])
+    [Input(component_id='component_id='dropdown-statistics', component_property='value'), Input('select-year', component_property='value')])
 
 def update_output_container(selected_statistics, input_year):
     if selected_statistics == 'Recession Period Statistics':
